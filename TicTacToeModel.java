11cb1ee9bbf7c91375fcc994d85433467a40ed58
jlidwall <123171109+jlidwall@users.noreply.github.com>
2023-11-06 15:25:16 +0100

    Add files via upload

--- TicTacToeModel.java
+++ TicTacToeModel.java
@@ -3,6 +3,7 @@ public class TicTacToeModel implements Boardgame{
     private String[][] board;
     private String message;
     private boolean firstMove;
+    private String turn;
     private int moveCount;
 
     public TicTacToeModel(){
@@ -10,15 +11,28 @@ public class TicTacToeModel implements Boardgame{
         message = "Player one's turn";
         firstMove = true;
         moveCount = 0;
+        turn = "X";
     }
 
     @Override
     public boolean move(int x, int y) {
 
+        String status = getStatus(x, y); 
+
         if (moveCount < 6) {   // utplaceringsfas
-            // kolla att det inte är mittpunkten
+            if (status == null && !(x == 1 && y == 1)) { // empty square and not center square
+                board[x][y] = turn; // place pawn
+                switchturn();
+                moveCount += 1;
+                return true;
+            }
+            else {
+                message = "Felaktigt drag";
+                return false;
+            }
             // kolla att det inte ligger en spelpjäs där
             // placera ut spelpjäs och returnera true
+
         }
         else { // flyttfas
             // kolla vilken spelares tur det är
