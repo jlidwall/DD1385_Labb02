0161b40e5bb068f11a46ea7f140d5b9062bfa7c3
jlidwall <123171109+jlidwall@users.noreply.github.com>
2023-11-06 16:12:17 +0100

    Add files via upload

--- TicTacToeModel.java
+++ TicTacToeModel.java
@@ -2,46 +2,74 @@ public class TicTacToeModel implements Boardgame{
 
     private String[][] board;
     private String message;
-    private boolean firstMove;
-    private String turn;
+    private boolean firstStep;
+    private String pawn;
     private int moveCount;
+    private int[] prevClick = new int[2];
 
     public TicTacToeModel(){
         board = new String[3][3];
         message = "Player one's turn";
-        firstMove = true;
+        firstStep = true;
         moveCount = 0;
-        turn = "X";
+        pawn = "X";
+        // initialize board
+        for (int i = 0; i < 3; i++){
+             for (int j = 0; j < 3; j++){
+                 board[i][j] = " ";
+             }
+        }
     }
 
     @Override
     public boolean move(int x, int y) {
 
-        String status = getStatus(x, y); 
+        String status = getStatus(x, y);
 
         if (moveCount < 6) {   // utplaceringsfas
-            if (status == null && !(x == 1 && y == 1)) { // empty square and not center square
-                board[x][y] = turn; // place pawn
-                switchturn();
+            if (status == " " && !(x == 1 && y == 1)) { // empty square and not center square
+                board[x][y] = pawn; // place pawn
+                switchTurn();
                 moveCount += 1;
                 return true;
             }
-            else {
-                message = "Felaktigt drag";
+            else {  // non legal move
+                message = "Please choose an empty position on the edge of the board";
                 return false;
             }
-            // kolla att det inte ligger en spelpjäs där
-            // placera ut spelpjäs och returnera true
-
         }
         else { // flyttfas
-            // kolla vilken spelares tur det är
-            // kolla först att en korrekt spelpjäs är vald
-            // kolla sedan att en tom ruta trycks som är granne 
-            // slutligen flytta spelpjäs och returna true
-        }
-
-    }
+            // X or O depending on hows playing, p1 = X, p2 = O
+           if (firstStep){
+               if (status != pawn) { // not choosen the right pawn
+                   message = "Please choose a pawn of type: " + pawn;
+                   return false;
+               }
+               
+               else{ // has choosen the right pawn
+                   prevClick[0] = x; // Store current click to prevois click so we can use it with in the next click
+                   prevClick[1] = y;
+                   firstStep = false;
+                   message = "Choose an empty square";
+                   return true;
+               } 
+           }
+           else{ // not the first move, means its time to choose an emtpy sqaure
+               if(status != " "){ // a square that already has a pawn placed is clicked
+                   message = "Please choose an empty square";
+                   return false;
+               }
+               else{ // gets here if a empty square is pressed
+                   board[x][y] = pawn; // swap squares between prevClick and current click
+                   board[prevClick[0]][prevClick[1]] = " ";
+                   firstStep = true;
+                   switchTurn();
+                   return true;
+               }
+               
+           }
+       }
+   }
 
     @Override
     public String getStatus(int x, int y) {
@@ -57,5 +85,15 @@ public class TicTacToeModel implements Boardgame{
     public String getMessage() {
         return message;
     }
-    
+
+    private void switchTurn(){
+        if (pawn == "X"){
+            pawn ="O";
+            message = "Player two's turn";
+        }
+        else{
+            pawn = "X";
+            message = "Player one's turn";
+        }
+    }
 }
